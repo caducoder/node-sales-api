99717c3135ec0e064b6280a395df842ecad88ca5
caducoder <cadudrop@hotmail.com>
2024-09-21 21:48:10 -0300

    fix: role name standardize

--- src/modules/users/services/RoleService.ts
+++ src/modules/users/services/RoleService.ts
@@ -1,7 +1,14 @@
 import userRoleRepository from "../repositories/userRoleRepository.js";
+import createHttpError from "http-errors";
 
 async function CreateNewRole(roleName: string) {
-  const newRole = await userRoleRepository.createRole(roleName);
+  if (roleName.length < 3) {
+    throw createHttpError(400, "Role name must be at least 3 characters long");
+  }
+
+  const newRole = await userRoleRepository.createRole(
+    roleName.toLocaleLowerCase()
+  );
   return newRole;
 }
 
